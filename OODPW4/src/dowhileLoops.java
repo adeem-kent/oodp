87df0ffe73bc373f4d9d80e766de79fc1822bfcb
adeemali <adeemanwer@gmail.com>
2024-10-17 10:32:07 +1100

    new

---
+++ OODPW4/src/dowhileLoops.java
@@ -0,0 +1,14 @@
+
+public class dowhileLoops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 10;
+		
+		do {
+			System.out.println(num);
+			num --;
+		} while (num >= 10);
+	}
+
+}
---
+++ OODPW4/src/whileLoops.java
@@ -0,0 +1,44 @@
+
+public class whileLoops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//infinite loop
+/*		int num = 10;
+		
+		while (num > 1)
+		{
+			System.out.println(num);
+		}
+*/		
+		////////////prints from 10 to 1
+/*		int num = 10;
+		
+		while (num >= 1)
+		{
+			System.out.println(num);
+			num = num - 1;
+		}
+*/		
+		////////////prints from 1 to 10
+/*		int number = 1;
+		
+		while (number <= 10)
+		{
+			System.out.println(number);
+			number = number + 1;
+		}
+*/		
+		
+		int number = 1;
+		
+		while (number <= 10)
+		{
+			System.out.println(number);
+			number ++;
+		}
+		
+	}
+
+}
