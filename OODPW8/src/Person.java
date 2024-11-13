beea65f4e06cef54d644d5a1f5490367067a567c
adeemali <adeemanwer@gmail.com>
2024-11-14 10:21:02 +1100

    new

---
+++ OODPW8/src/Person.java
@@ -0,0 +1,17 @@
+
+public class Person {
+
+	// instance variables
+	int id;
+	public String name;
+	public String address;
+	public double number;
+
+	///// instance method
+	public void display() {
+		System.out.println("ID: " + id);
+		System.out.println("Name: " + name);
+		System.out.println("Address: " + address);
+		System.out.println("Number: " + number);
+	}
+}
